197558c0ad7eb6e5a0b13d94610098036ebc91e1
Aspero17 <mark_y6.53.5@mail.ru>
2024-09-27 22:33:29 +0300

    v3.1

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import logging  # Импорт для настройки логирования
 from aiogram import Bot, Dispatcher  # Импорт для работы с ботом и обработчиками сообщений
 from aiogram.fsm.storage.memory import MemoryStorage  # Импорт для хранения состояний в памяти
-from handlers import registration, profile, goods, general, cart, editcart, admin # Импорт обработчиков для различных функциональностей
+from handlers import registration, profile, goods, general, cart, editcart  # Импорт обработчиков для различных функциональностей
 from config import API_TOKEN  # Импорт токена API для бота
 from handlers.feedback import router as feedback_router  # Импорт маршрутизатора для обработки обратной связи
 from utils.database import initialize_db  # Инициализации базы данных 123156165
 from data.usersdb import initialize_users_db
-#from handlers.admin import router as admin_router
+from handlers.admin import router as admin_router
 # Настройка базового уровня логирования для отслеживания информации
 logging.basicConfig(level=logging.INFO)
 
@@ -25,7 +25,7 @@ dp.include_router(general.router)  # Обработчик для общих ко
 
 dp.include_router(editcart.router)
 dp.include_router(cart.router)
-dp.include_router(admin.router)
+dp.include_router(admin_router)
 
 # Инициализация базы данных
 initialize_db()
